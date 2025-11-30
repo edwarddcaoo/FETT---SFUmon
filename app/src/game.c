26d855de4d42b5335aba5199be44768b6837ea2f
Edward Cao <edwarddcaoo@gmail.com>
2025-11-29 22:17:39 -0800

    mini changes to audio

--- app/src/game.c
+++ app/src/game.c
@@ -22,7 +22,6 @@ void game_run(void) {
     // Load sound effects
     printf("Loading sound effects...\n");
     audio_load_sound("assets/sounds/catch.wav", SOUND_CATCH);
-    audio_load_sound("assets/sounds/door.wav", SOUND_DOOR);
     
     // Initialize game systems
     Map game_map;
@@ -80,9 +79,6 @@ void game_run(void) {
         if (input_is_catch_pressed(&space_was_pressed)) {
             printf("Space pressed! Checking for professors...\n");
             
-            // Play sound immediately
-            audio_play_sound(SOUND_CATCH);
-            
             // Then check if actually caught
             npc_try_catch(current_room->npcs, current_room->npc_count, 
                          player_get_grid_x(&player), 
@@ -101,9 +97,6 @@ void game_run(void) {
                                                   player_get_grid_x(&player), 
                                                   player_get_grid_y(&player));
             if (door != NULL) {
-                // Play door sound
-                audio_play_sound(SOUND_DOOR);
-                
                 int new_x = player.grid_x;
                 int new_y = player.grid_y;
                 map_transition_room(&game_map, door->target_room, &new_x, &new_y, door);
